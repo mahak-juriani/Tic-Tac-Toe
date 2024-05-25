4fc9f0818fb03fcfbf38397602b4a0bad2492bd5
Mahak Juriani <juriani.mahak@gmail.com>
2024-05-26 03:44:39 +0530

    log test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import org.example.models.*;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) throws InvalidMoveException {
         System.out.println("Hello world!");
